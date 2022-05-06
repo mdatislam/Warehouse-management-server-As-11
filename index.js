a31cda98e858f3c921ef796e83c8ec98342f5b37
unknown <mdatislam>
2022-05-06 09:50:46 +0600

    update api implement

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-require('dotenv').config()
+require("dotenv").config();
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
@@ -9,67 +9,71 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 app.use(cors());
 app.use(express.json());
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.agxjr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
- async function run(){
-    try{
-        await client.connect();
-        const productCollection = client.db('warehouse2').collection('products');
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("warehouse2").collection("products");
 
-        // All products Load from bd 
-        app.get('/products', async (req,res)=>{
-          const query={};
-          const cursor= productCollection.find(query)
-          const result= await cursor.toArray()
-          res.send(result)
-        });
-        // search required product from db 
-        app.get('/products/:id', async(req,res)=>{
-          const id = req.params.id
-          const query= {_id:ObjectId(id)} 
-          const result = await productCollection.findOne(query)
-          res.send(result)
-        })
-          // Product upload to db
-        app.post('/products', async (req,res)=>{
-            const data= req.body
-            const product = await productCollection.insertOne(data)
-            res.send(product)
-        });
-        // selected product delete 
-         app.delete('/products/:id', async(req,res)=>{
-         const id= req.params.id;
-         const query = {_id:ObjectId(id)}
-         const result= await productCollection.deleteOne(query)
-         res.send(result)
-        }); 
-        // Selected product update
-        app.put('/products/:id',async(req,res)=>{
-          const id = req.params.id
-          const updateProduct= req.body
-          const filter = {_id:ObjectId(id)}
-          const options = { upsert: true };
-          const updateDoc = {
-            $set: {
-              name: updateProduct.name,
-              price:updateProduct.price
-            },
-          };
+    // All products Load from bd
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    // search required product from db
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
 
-          const result= await productCollection.updateOne(filter,updateDoc,options)
-          res.send(result)
-        })
+    // Product upload to db
+    app.post("/products", async (req, res) => {
+      const data = req.body;
+      const product = await productCollection.insertOne(data);
+      res.send(product);
+    });
+    // selected product delete
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    });
+    // Selected product update
+    app.put("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateProduct = req.body;
+      console.log(updateProduct)
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          Stock:updateProduct.newStock,
+         
+        },
+      };
 
-    }
-    finally{
-
-    }
-
+      const result = await productCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+  } finally {
+  }
 }
-run().catch(console.dir); 
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send(" Hello electronic warehouse Management");
